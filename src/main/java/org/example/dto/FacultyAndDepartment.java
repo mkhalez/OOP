0ef31d4109caec306ab10192a104bdd2dd8a152f
mkhalez <mikhalko0606@mail.ru>
2026-02-14 14:43:24 +0300

    Последнаяя версия

--- src/main/java/org/example/dto/FacultyAndDepartment.java
+++
@@ -1,7 +0,0 @@
-package org.example.dto;
-
-import org.example.entities.Department;
-import org.example.entities.Faculty;
-
-public record FacultyAndDepartment(Faculty faculty, Department department) {
-}
--- src/main/java/org/example/dto/FacultyAndDepartmentNames.java
+++
@@ -1,4 +0,0 @@
-package org.example.dto;
-
-public record FacultyAndDepartmentNames(String facultyName, String departmentName) {
-}
